fe718e93a8ea0c62be7829a7478ae7cae5d7131d
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-02-24 20:49:03 +0100

    feat: counter projekt programmiert

--- src/Projects/Project5.jsx
+++ src/Projects/Project5.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function Project5() {
-  return <h1>Project 5</h1>;
+  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(1);
+
+  return (
+    <>
+      <div>
+        <p>
+          Your Count is: <span>{count}</span>
+        </p>
+        <button onClick={() => setCount(count + amount)}>+</button>
+        <button onClick={() => setCount(count - amount)}>-</button>
+        <button onClick={() => setCount(0)}>reset</button>
+      </div>
+    </>
+  );
 }
 export default Project5;
