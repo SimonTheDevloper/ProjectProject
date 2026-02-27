88ac253d37c1691a3269a4eadd41578a472dc7b0
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-02-27 20:58:01 +0100

    feat: design für den counter programmiert und noch mehr aktionen

--- src/Projects/Project5.jsx
+++ src/Projects/Project5.jsx
@@ -4,49 +4,57 @@ import CounterBtn from "./CounterBtn.jsx";
 function Project5() {
   const [count, setCount] = useState(0);
   const [amount, setAmount] = useState(1);
+  const [error, setError] = useState(null);
+  const maxLimitAmount = 100;
 
-  function increase() {
-    setCount(count + amount);
-  }
+  const BtnListe = [
+    { label: "+", action: () => setCount((a) => a + 1) },
+    { label: "-", action: () => setCount((a) => a - 1) },
+    { label: "reset", action: () => setCount(0) },
+    { label: "+10", action: () => setCount((a) => a + 10) },
+    { label: "-10", action: () => setCount((a) => a - 10) },
+    { label: "x 10", action: () => setCount((a) => a * 10) },
+    { label: "/ 10", action: () => setCount((a) => a / 10) },
+    {
+      label: "random",
+      action: () => setCount(Math.floor(Math.random() * 100)),
+    },
+    { label: "half", action: () => setCount((a) => a / 2) },
+    { label: "round off", action: () => setCount((a) => Math.abs(a)) },
+    { label: "round up", action: () => setCount((a) => Math.ceil(a)) },
+    { label: "10%", action: () => setCount((a) => a * 0.1) },
+  ];
+  const handleOnChange = (e) => {
+    const value = Number(e.target.value);
 
-  function decrease() {
-    setCount(count - amount);
-  }
-
-  function reset() {
-    setCount(0);
-  }
-
-  function double() {
-    setCount(count * 2);
-  }
-  function square() {
-    setCount(count * count);
-  }
-
+    if (value > maxLimitAmount) {
+      setError(`Stop! max Amount is ${maxLimitAmount}`);
+      setAmount(maxLimitAmount);
+    } else {
+      setError("");
+      setAmount(value);
+    }
+  };
   return (
-    <div className="counter">
-      <p>
-        Your Count is: <span>{count}</span>
-      </p>
+    <main className="prot5">
+      <div className="counter">
+        <p>
+          Your Count is: <span>{count}</span>
+        </p>
 
-      <div className="buttons">
-        <CounterBtn text="+" onClick={increase} />
-        <CounterBtn text="-" onClick={decrease} />
-        <CounterBtn text="Reset" onClick={reset} />
-        <CounterBtn text="double" onClick={double} />
-        <CounterBtn text="toSquare" onClick={square} />
-      </div>
+        <div className="buttons">
+          {BtnListe.map((item, index) => (
+            <CounterBtn key={index} text={item.label} onClick={item.action} />
+          ))}
+        </div>
 
-      <div className="amount">
-        <p>Step size:</p>
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-        />
+        <div className="amount">
+          <p>Step size:</p>
+          <input type="number" value={amount} onChange={handleOnChange} />
+          {error && <p>{error}</p>}
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
